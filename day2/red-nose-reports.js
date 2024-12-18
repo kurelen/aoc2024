de713c9502d190b74da608b9e429cd6c4d59a4ec
Alexander Jandt <kurelen@pm.me>
2024-12-18 11:50:22 +0100

    Fix day2 part two

--- day2/red-nose-reports.js
+++ day2/red-nose-reports.js
@@ -24,22 +24,18 @@ function into_lists(reports, line) {
 	return reports;
 }
 
-function error_index(report) {
+function safe_report(report) {
 	if (report.length <= 1) {
-    return -1;
+    return true;
 	}
 	let signum = report[0] - report[1] < 0 ? -1 : 1;
 	for (let i = 1; i < report.length; i++) {
 		const diff = signum * (report[i-1] - report[i]);
 		if (diff <= 0 || diff > 3) {
-			return i;
+			return false;
 		}
 	}
-	return -1;
-}
-
-function safe_report(report) {
-	return error_index(report) === -1;
+	return true;
 }
 
 function remove_nth(xs, n) {
@@ -47,13 +43,9 @@ function remove_nth(xs, n) {
 }
 
 function damped_safe_report(report) {
-	const error_idx = error_index(report);
-	if (error_idx === -1) {
-    return true;
-	} else {
-    return safe_report(remove_nth(report, error_idx))
-		  || safe_report(remove_nth(report, error_idx - 1));
-	}
+	return safe_report(report)
+		|| report.some((_, idx) =>
+			safe_report(remove_nth(report, idx)));
 }
 
 function process_part_one(reports) {
