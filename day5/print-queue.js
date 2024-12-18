d1be5c8d819f746f642e8bc6416d54a08525dca7
Alexander Jandt <kurelen@pm.me>
2024-12-18 19:01:21 +0100

    Solve day 5 part one

--- day5/print-queue.js
+++ day5/print-queue.js
@@ -16,21 +16,60 @@ async function process_input(
   return initialValue;
 }
 
-function into_lines(value, line) {
-	value.push(line);
-	return value;	
+function to_ints(ss) {
+	return ss.map(s => parseInt(s, 10));
 }
 
-function process_part_one(lines) {
+function into_queue(queue, line) {
+	if (line.includes("|")) {
+		const [smaller, bigger] = to_ints(line.split("|"));
+		if (smaller in queue.rules) {
+			queue.rules[smaller].add(bigger);
+		} else {
+			queue.rules[smaller] = new Set([bigger]);
+		}
+	} else if (line.includes(",")) {
+		queue.updates.push(to_ints(line.split(",")));
+	}
+	return queue;	
+}
+
+function update_is_valid(rules) {
+	return (update) => {
+	  for (let i = 0; i < update.length; i++) {
+	  	for (let j = 0; j < update.length; j++) {
+				if ( (i < j && rules[update[j]]?.has(update[i]))
+				  || (i > j && rules[update[i]]?.has(update[j]))) {
+          return false;
+		  	}
+		  }
+	  }
+	  return true;
+	};
+}
+
+function add_middle(result, update) {
+	const mid = (update.length - 1) / 2;
+	if (update.length % 2 === 0) {
+    console.warn("Even length: ", update);
+		return result;
+	}
+  return result + update[mid];
+}
+
+function process_part_one(queue) {
+	const { updates, rules } = queue;
+	return updates
+		.filter(update_is_valid(rules))
+	  .reduce(add_middle, 0);
+}
+
+function process_part_two(queue) {
 	return false;
 }
 
-function process_part_two(lines) {
-	return false;
-}
-
-process_input("input", into_lines, [])
-  .then((lines) => {
-    console.log("Solution part one: ", process_part_one(lines));
-    console.log("Solution part two: ", process_part_two(lines));
+process_input("input", into_queue, { rules: {}, updates: []})
+  .then((queue) => {
+    console.log("Solution part one: ", process_part_one(queue));
+    console.log("Solution part two: ", process_part_two(queue));
   })
