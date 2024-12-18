fc6b456831aff888b6324600fbee8aac2bcc5dc7
Alexander Jandt <kurelen@pm.me>
2024-12-18 20:26:45 +0100

    Solve day6 part one

--- day6/guard-gallivant.js
+++ day6/guard-gallivant.js
@@ -23,7 +23,10 @@ function into_state(state, line) {
 	for (let i = 0; i < line.length; i++) {
 		const c = line[i];
 		if (c === "#") {
-      state.obstacles.push([state.rows, i]);
+			if (!(state.rows in state.obstacles)) {
+        state.obstacles[state.rows] = {};
+			}
+			state.obstacles[state.rows][i] = true; 
 		} else if (c === "^") {
       state.guard = [state.rows, i];
 		}
@@ -37,17 +40,45 @@ function turn(direction) {
 	return [dy, -dx];
 }
 
-function process_part_one(lines) {
-	return false;
+function guard_in_map(state) {
+  const [row, column] = state.guard;
+	const { rows, columns } = state;
+	return 0 <= row && row < rows
+		&& 0 <= column && column < columns;
+}
+
+function to_coordinate(xs) {
+	return xs.join("-");
+}
+
+function process_part_one(state) {
+	const path = new Set();
+	while (guard_in_map(state)) {
+		const [guard_r, guard_c] = state.guard;
+		const [d_r, d_c] = state.direction;
+		const next = [guard_r + d_r, guard_c + d_c];
+		const [n_r, n_c] = next;
+		if (state.obstacles?.[n_r]?.[n_c]) {
+      state.direction = turn(state.direction);
+			console.log(`# Guard hits obstacle at ${next}`);
+			console.log(`> Guard turns ${state.direction}`);
+		} else {
+			console.log(`Guard moves to ${next}`);
+			path.add(to_coordinate(next));
+			state.guard = next;
+		}
+	}
+	return path.size;
 }
 
 function process_part_two(lines) {
 	return false;
 }
 
-process_input("input", into_state, { rows: 0, columns: 0, obstacles: [], guard: undefined, direction: [-1, 0]})
+process_input("input",
+	into_state,
+	{ rows: 0, columns: 0, obstacles: {}, guard: undefined, direction: [-1, 0]})
   .then((state) => {
-		console.log(state)
     console.log("Solution part one: ", process_part_one(state));
     console.log("Solution part two: ", process_part_two(state));
   })
