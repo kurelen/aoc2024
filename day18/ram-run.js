078fbd8d99f7100022427382f72ffc8e6aeffbc3
Alexander Jandt <kurelen@pm.me>
2024-12-18 19:14:17 +0100

    Prepare templates and input

---
+++ day18/ram-run.js
@@ -0,0 +1,36 @@
+const fs = require("node:fs");
+const readline = require("node:readline");
+
+async function process_input(
+  filename,
+  reducer,
+  initialValue
+) {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(filename),
+    crlfDelay: Infinity,
+  });
+  for await (const line of rl) {
+    initialValue = reducer(initialValue, line);
+  }
+  return initialValue;
+}
+
+function into_lines(value, line) {
+	value.push(line);
+	return value;	
+}
+
+function process_part_one(lines) {
+	return false;
+}
+
+function process_part_two(lines) {
+	return false;
+}
+
+process_input("input", into_lines, [])
+  .then((lines) => {
+    console.log("Solution part one: ", process_part_one(lines));
+    console.log("Solution part two: ", process_part_two(lines));
+  })
