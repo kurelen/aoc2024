44bdb49856e8478e2c1cdbc5ee61bd142d1b1887
Alexander Jandt <kurelen@pm.me>
2024-12-18 19:57:58 +0100

    Parse and turn

--- day6/guard-gallivant.js
+++ day6/guard-gallivant.js
@@ -16,9 +16,25 @@ async function process_input(
   return initialValue;
 }
 
-function into_lines(value, line) {
-	value.push(line);
-	return value;	
+function into_state(state, line) {
+	if (state.rows === 0) {
+    state.columns = line.length;
+	}
+	for (let i = 0; i < line.length; i++) {
+		const c = line[i];
+		if (c === "#") {
+      state.obstacles.push([state.rows, i]);
+		} else if (c === "^") {
+      state.guard = [state.rows, i];
+		}
+	}
+	state.rows++;
+	return state;
+}
+
+function turn(direction) {
+  const [dx, dy] = direction;
+	return [dy, -dx];
 }
 
 function process_part_one(lines) {
@@ -29,8 +45,9 @@ function process_part_two(lines) {
 	return false;
 }
 
-process_input("input", into_lines, [])
-  .then((lines) => {
-    console.log("Solution part one: ", process_part_one(lines));
-    console.log("Solution part two: ", process_part_two(lines));
+process_input("input", into_state, { rows: 0, columns: 0, obstacles: [], guard: undefined, direction: [-1, 0]})
+  .then((state) => {
+		console.log(state)
+    console.log("Solution part one: ", process_part_one(state));
+    console.log("Solution part two: ", process_part_two(state));
   })
