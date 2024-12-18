f9d1076ad42560a17d72b74b3946f83fdc2b75bf
Alexander Jandt <kurelen@pm.me>
2024-12-18 09:13:09 +0100

    Solution day2 part 2

--- day2/red-nose-reports.js
+++ day2/red-nose-reports.js
@@ -24,18 +24,36 @@ function into_lists(reports, line) {
 	return reports;
 }
 
-function safe_report(report) {
+function error_index(report) {
 	if (report.length <= 1) {
-    return true;
+    return -1;
 	}
 	let signum = report[0] - report[1] < 0 ? -1 : 1;
 	for (let i = 1; i < report.length; i++) {
 		const diff = signum * (report[i-1] - report[i]);
 		if (diff <= 0 || diff > 3) {
-			return false;
+			return i;
 		}
 	}
-	return true;
+	return -1;
+}
+
+function safe_report(report) {
+	return error_index(report) === -1;
+}
+
+function remove_nth(xs, n) {
+  return xs.filter((_, idx) => idx !== n);
+}
+
+function damped_safe_report(report) {
+	const error_idx = error_index(report);
+	if (error_idx === -1) {
+    return true;
+	} else {
+    return safe_report(remove_nth(report, error_idx))
+		  || safe_report(remove_nth(report, error_idx - 1));
+	}
 }
 
 function process_part_one(reports) {
@@ -43,7 +61,7 @@ function process_part_one(reports) {
 }
 
 function process_part_two(reports) {
-	return 0;
+	return reports.filter(damped_safe_report).length;
 }
 
 process_input("input", into_lists, [])
