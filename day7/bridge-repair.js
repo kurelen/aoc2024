9fd0be94d44550e92ef60317231abbeabd54231e
Alexander Jandt <kurelen@pm.me>
2024-12-19 15:08:14 +0100

    Solve day7 part two

--- day7/bridge-repair.js
+++ day7/bridge-repair.js
@@ -36,22 +36,13 @@ function is_solvable({ aim, values }) {
 	}
 	const last = values.at(-1);
 	values = values.slice(0, -1);
-	if (aim % last === 0) {
-    return is_solvable({
-			aim: aim - last, 
-			values
-		}) || is_solvable({
-			aim: aim / last,
-			values
-		});
-
-	} else {
-    return is_solvable({
-			aim: aim - last,
-			values
-		});
-	}
-  return true;
+	return (aim % last === 0 && is_solvable({
+		aim: aim / last,
+		values
+	})) || is_solvable({
+		aim: aim - last,
+		values
+	});
 }
 
 function process_part_one(problems) {
@@ -60,8 +51,50 @@ function process_part_one(problems) {
 	  .reduce((acc, { aim }) => acc + aim, 0);
 }
 
-function process_part_two(lines) {
-	return false;
+function is_concatenated(num, to_test) {
+	return num.toString().endsWith(to_test.toString());
+	return result;
+}
+
+function split_end(num, to_test) {
+	const result = parseInt(
+		num.toString().slice(
+			0,
+			-1 * to_test.toString().length
+		),
+		10);
+	return result;
+}
+
+function is_solvable_2({ aim, values }) {
+	if (values.length === 0) {
+    return false;
+	}
+	if (values.length === 1 && aim === values[0]) {
+    return true;
+	}
+	const last = values.at(-1);
+	values = values.slice(0, -1);
+	return (aim % last === 0
+		&& is_solvable_2({
+			aim: aim / last,
+			values
+		}))
+		|| (is_concatenated(aim, last)
+			&& is_solvable_2({
+				aim: split_end(aim, last),
+				values
+		})) || is_solvable_2({
+		aim: aim - last,
+		values
+	});
+}
+
+
+function process_part_two(problems) {
+	return problems
+		.filter(is_solvable_2)
+	  .reduce((acc, { aim }) => acc + aim, 0);
 }
 
 process_input("input", into_problems, [])
